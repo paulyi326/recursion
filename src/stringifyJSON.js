843937d39619adb745916d3f566a51920b1575ff
Paul Yi <paul.yi326@gmail.com>
2014-05-19 18:08:26 -0700

    works when input is only a primitive

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,17 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
+ 
   // your code goes here
+ 
+  var type = typeof obj;
+  // if obj is just a primitive
+  if (type !== "object" || obj === null) {
+    if (type == "string") {
+      obj = '"' + obj + '"';
+    }
+    return String(obj);
+  } else if (type === "object") {
+
+  }
 };
