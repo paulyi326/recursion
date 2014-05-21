74c03a76be9594634afbffc1a18417d16b9ffa6c
Paul Yi <paul.yi326@gmail.com>
2014-05-20 19:17:27 -0700

    first working draft of stringify. will clean clean it up later

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,12 +8,39 @@ var stringifyJSON = function(obj) {
  
   var type = typeof obj;
   // if obj is just a primitive
-  if (type !== "object" || obj === null) {
-    if (type == "string") {
-      obj = '"' + obj + '"';
+  if (type !== "function" && type !== "undefined") {
+    if (type !== "object" || obj === null) {
+      if (type == "string") {
+        obj = '"' + obj + '"';
+      }
+      return String(obj);
+    } else if (type === "object") {
+      var objIsArray = Array.isArray(obj);
+      var result = [];
+      for (var key in obj) {
+        var value = obj[key];
+        if (typeof value !== "function" && typeof value !== "undefined") {
+          var valueType = typeof value;
+          if (valueType === "string") {
+            value = '"' + value + '"';
+          } else if (valueType === "object") { 
+            value = stringifyJSON(value);
+          }
+          if (!objIsArray) {
+            result.push(('"' + key + '":' + String(value)));
+          } else {
+            result.push(String(value));
+          }
+        }
+      }   
+      if (objIsArray) {
+        return "[" + String(result) + "]";  
+      } else {
+        return "{" + String(result) + "}";
+      }
+         
     }
-    return String(obj);
-  } else if (type === "object") {
-
+  } else {
+    return undefined;
   }
 };
