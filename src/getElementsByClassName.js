5abb782e27e157dba8ee8a7ee3487c553c118272
Paul Yi <paul.yi326@gmail.com>
2014-05-21 13:01:23 -0700

    first working version.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,28 +6,50 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
-  var targetClasses = className.split(" ");
-  return getElementsHelper(targetClasses, document.body, [], 1);
+
+  var targetClasses = className.split(" "); // array of all classes user is searching for
+  return getElementsHelper(targetClasses, document.body, []);
 };
 
-var getElementsHelper = function(targetClasses, currElement, elements, count) {
-  var classList = currElement.classList;
-  var children = currElement.childNodes;
-  var matches = 0;
-  for (var i = 0; i < targetClasses.length; i++) {
-    if (classList.contains(targetClasses[i])) {
-      console.log(classList + count);
-      count++;
-      matches++;
+// helper method so that I can pass in more parameters
+var getElementsHelper = function(targetClasses, currElement, elements) {
+    if (currElement !== null) {
+      var classList = currElement.classList;
+
+      // removing text nodes so only element nodes remain
+      var children = filter(currElement.childNodes, function(child) {
+        return child.nodeType === Node.ELEMENT_NODE;
+      });
+      
+      // I think this chunk of code makes it so this function would also work 
+      // for searching for elements with multiple class names
+      var matches = 0;
+      for (var i = 0; i < targetClasses.length; i++) {
+        if (classList.contains(targetClasses[i])) {
+          matches++;
+        }
+      }  
+      // if element has all of the target classes
+      if (matches === targetClasses.length) {
+        elements.push(currElement);
+      }
+
+      // recursive call on all of the current node's children
+      for (var i = 0; i < children.length; i++) {
+        getElementsHelper(targetClasses, children[i], elements);
+      }
     }
-  }  
-  if (matches === targetClasses.length) {
-    elements.push(currElement);
-  }
-
-  for (var i = 0; i < children.length; i++) {
-    getElementsHelper(targetClasses, children[i], elements);
-  }
-  return elements;
+    return elements;
 
 };
+
+// helper function that filters based on test function
+var filter = function(arr, test) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (test(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
